22549f1b5b8e357710a86d2e320bebf81ea2f4f2
Anıl Goktas <anilgkts@outlook.com.tr>
2018-11-17 13:41:27 +0300

    test
    test

--- AkilliEntegre/HBSection.cs
+++ AkilliEntegre/HBSection.cs
@@ -21,6 +21,7 @@ namespace AkilliEntegre
     {
         Functions func = new Functions();
         HBAPi hb = new HBAPi();
+        Export exp = new Export();
 
         private String userName = "akilliphone_dev";
         private String password = "Ak12345!";
@@ -158,31 +159,7 @@ namespace AkilliEntegre
 
         private void xlsxExport_Click(object sender, EventArgs e)
         {
-            copyAlltoClipboard();
-        }
-
-        public void ExportExcell()
-        {
-            copyAlltoClipboard();
-            Microsoft.Office.Interop.Excel.Application xlexcel;
-            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
-            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
-            object misValue = System.Reflection.Missing.Value;
-            xlexcel = new Microsoft.Office.Interop.Excel.Application();
-            xlexcel.Visible = true;
-            xlWorkBook = xlexcel.Workbooks.Add(misValue);
-            xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-            Microsoft.Office.Interop.Excel.Range CR = (Microsoft.Office.Interop.Excel.Range)xlWorkSheet.Cells[1, 1];
-            CR.Select();
-            xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
-        }
-
-        private void copyAlltoClipboard()
-        {
-            metroGrid1.SelectAll();
-            DataObject dataObj = metroGrid1.GetClipboardContent();
-            if (dataObj != null)
-                Clipboard.SetDataObject(dataObj);
+            exp.ExcellExport(metroGrid1);
         }
 
     }
